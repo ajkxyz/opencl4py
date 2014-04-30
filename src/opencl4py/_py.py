5c90bba8136a807e6b82f352001fc07f778be199
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-04-30 10:29:36 +0400

    Added OpenCL error names based on #define-s from cl.h

--- src/opencl4py/_py.py
+++ src/opencl4py/_py.py
@@ -52,6 +52,70 @@ class CL(object):
         _lib: handle to cffi.FFI object.
         _handle: cffi handle to OpenCL object.
     """
+
+    ERRORS = {
+        0:   "CL_SUCCESS",
+        -1:  "CL_DEVICE_NOT_FOUND",
+        -2:  "CL_DEVICE_NOT_AVAILABLE",
+        -3:  "CL_COMPILER_NOT_AVAILABLE",
+        -4:  "CL_MEM_OBJECT_ALLOCATION_FAILURE",
+        -5:  "CL_OUT_OF_RESOURCES",
+        -6:  "CL_OUT_OF_HOST_MEMORY",
+        -7:  "CL_PROFILING_INFO_NOT_AVAILABLE",
+        -8:  "CL_MEM_COPY_OVERLAP",
+        -9:  "CL_IMAGE_FORMAT_MISMATCH",
+        -10: "CL_IMAGE_FORMAT_NOT_SUPPORTED",
+        -11: "CL_BUILD_PROGRAM_FAILURE",
+        -12: "CL_MAP_FAILURE",
+        -13: "CL_MISALIGNED_SUB_BUFFER_OFFSET",
+        -14: "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST",
+        -15: "CL_COMPILE_PROGRAM_FAILURE",
+        -16: "CL_LINKER_NOT_AVAILABLE",
+        -17: "CL_LINK_PROGRAM_FAILURE",
+        -18: "CL_DEVICE_PARTITION_FAILED",
+        -19: "CL_KERNEL_ARG_INFO_NOT_AVAILABLE",
+
+        -30: "CL_INVALID_VALUE",
+        -31: "CL_INVALID_DEVICE_TYPE",
+        -32: "CL_INVALID_PLATFORM",
+        -33: "CL_INVALID_DEVICE",
+        -34: "CL_INVALID_CONTEXT",
+        -35: "CL_INVALID_QUEUE_PROPERTIES",
+        -36: "CL_INVALID_COMMAND_QUEUE",
+        -37: "CL_INVALID_HOST_PTR",
+        -38: "CL_INVALID_MEM_OBJECT",
+        -39: "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR",
+        -40: "CL_INVALID_IMAGE_SIZE",
+        -41: "CL_INVALID_SAMPLER",
+        -42: "CL_INVALID_BINARY",
+        -43: "CL_INVALID_BUILD_OPTIONS",
+        -44: "CL_INVALID_PROGRAM",
+        -45: "CL_INVALID_PROGRAM_EXECUTABLE",
+        -46: "CL_INVALID_KERNEL_NAME",
+        -47: "CL_INVALID_KERNEL_DEFINITION",
+        -48: "CL_INVALID_KERNEL",
+        -49: "CL_INVALID_ARG_INDEX",
+        -50: "CL_INVALID_ARG_VALUE",
+        -51: "CL_INVALID_ARG_SIZE",
+        -52: "CL_INVALID_KERNEL_ARGS",
+        -53: "CL_INVALID_WORK_DIMENSION",
+        -54: "CL_INVALID_WORK_GROUP_SIZE",
+        -55: "CL_INVALID_WORK_ITEM_SIZE",
+        -56: "CL_INVALID_GLOBAL_OFFSET",
+        -57: "CL_INVALID_EVENT_WAIT_LIST",
+        -58: "CL_INVALID_EVENT",
+        -59: "CL_INVALID_OPERATION",
+        -60: "CL_INVALID_GL_OBJECT",
+        -61: "CL_INVALID_BUFFER_SIZE",
+        -62: "CL_INVALID_MIP_LEVEL",
+        -63: "CL_INVALID_GLOBAL_WORK_SIZE",
+        -64: "CL_INVALID_PROPERTY",
+        -65: "CL_INVALID_IMAGE_DESCRIPTOR",
+        -66: "CL_INVALID_COMPILER_OPTIONS",
+        -67: "CL_INVALID_LINKER_OPTIONS",
+        -68: "CL_INVALID_DEVICE_PARTITION_COUNT",
+    }
+
     def __init__(self):
         self._lib = cl.lib  # to hold the reference
         self._handle = None
@@ -92,6 +156,14 @@ class CL(object):
             n_events = 0
             wait_list = cl.NULL
         return (wait_list, n_events)
+
+    @staticmethod
+    def get_error_name_from_code(code):
+        return CL.ERRORS.get(code, "UNKNOWN")
+
+    @staticmethod
+    def get_error_description(code):
+        return "%s (%d)" % (CL.get_error_name_from_code(code), code)
 
 
 class Event(CL):
@@ -366,14 +438,16 @@ class Queue(CL):
         """
         n = self._lib.clFlush(self.handle)
         if n:
-            raise CLRuntimeError("clFlush() failed with error %d" % (n), n)
+            raise CLRuntimeError("clFlush() failed with error %s" %
+                                 CL.get_error_description(n), n)
 
     def finish(self):
         """Waits for all previous commands issued to this queue to end.
         """
         n = self._lib.clFinish(self.handle)
         if n:
-            raise CLRuntimeError("clFinish() failed with error %d" % (n), n)
+            raise CLRuntimeError("clFinish() failed with error %s" %
+                                 CL.get_error_description(n), n)
 
     def release(self):
         if self.handle is not None:
@@ -630,9 +704,10 @@ class Program(CL):
                 (b"".join(log[0:sz[0] - 1])).decode("utf-8", "replace"))
         if n:
             raise CLRuntimeError(
-                "clBuildProgram() failed with error %d\n"
+                "clBuildProgram() failed with error %s\n"
                 "Logs are:\n%s\nSource was:\n%s\n" %
-                (n, "\n".join(self.build_logs), self.src), n)
+                (CL.get_error_description(n),
+                 "\n".join(self.build_logs), self.src), n)
 
     def release(self):
         if self.handle is not None:
@@ -893,14 +968,14 @@ class Platform(CL):
         n = self._lib.clGetDeviceIDs(handle, cl.CL_DEVICE_TYPE_ALL,
                                      0, cl.NULL, nn)
         if n:
-            raise CLRuntimeError("clGetDeviceIDs() failed "
-                                 "with error %d" % (n), n)
+            raise CLRuntimeError("clGetDeviceIDs() failed with error %s" %
+                                 CL.get_error_description(n), n)
         ids = cl.ffi.new("cl_device_id[]", nn[0])
         n = self._lib.clGetDeviceIDs(handle, cl.CL_DEVICE_TYPE_ALL,
                                      nn[0], ids, nn)
         if n:
-            raise CLRuntimeError("clGetDeviceIDs() failed "
-                                 "with error %d" % (n), n)
+            raise CLRuntimeError("clGetDeviceIDs() failed with error %s" %
+                                 CL.get_error_description(n), n)
         self._devices = list(Device(dev_id, self,
                                     "%s:%d" % (self.path, dev_num))
                              for dev_id, dev_num in zip(ids, range(len(ids))))
@@ -953,13 +1028,13 @@ class Platforms(CL):
         nn = cl.ffi.new("cl_uint[]", 1)
         n = self._lib.clGetPlatformIDs(0, cl.NULL, nn)
         if n:
-            raise CLRuntimeError("clGetPlatformIDs() failed "
-                                 "with error %d" % (n), n)
+            raise CLRuntimeError("clGetPlatformIDs() failed with error %s" %
+                                 CL.get_error_description(n), n)
         ids = cl.ffi.new("cl_platform_id[]", nn[0])
         n = self._lib.clGetPlatformIDs(nn[0], ids, nn)
         if n:
-            raise CLRuntimeError("clGetPlatformIDs() failed "
-                                 "with error %d" % (n), n)
+            raise CLRuntimeError("clGetPlatformIDs() failed with error %s" %
+                                 CL.get_error_description(n), n)
         self._platforms = list(Platform(p_id, str(p_num))
                                for p_id, p_num in zip(ids, range(len(ids))))
 
