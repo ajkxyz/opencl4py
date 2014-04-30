03e86e7d1f3669a689dbcc2f4fd57d4b59ffcb29
Alexey Kazantsev <a.kazantsev@samsung.com>
2014-04-30 15:36:11 +0400

    Changed version to 1.2.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ from distutils.core import setup
 setup(
     name="opencl4py",
     description="OpenCL cffi bindings and helper classes",
-    version="1.1.0",
+    version="1.2.0",
     license="Simplified BSD",
     author="Samsung Electronics Co.,Ltd.",
     author_email="a.kazantsev@samsung.com",
@@ -60,6 +60,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries"
     ]
 )
